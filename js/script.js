40d79a37e7a29ac435109ccdbdf5819e235e2f13
Danny <danny.brogno@gmail.com>
2021-04-19 18:57:24 +0200

    Milestone 1 done

--- js/script.js
+++ js/script.js
@@ -38,18 +38,18 @@ function vueInit() {
         .catch(() => console.log("I AM ERRROR"));
         // ----------------TV--------------------
         // --------------SERIES------------------
-        axios.get('https://api.themoviedb.org/3/search/tv',{
-          params: {
-            "api_key": "5417e301ed04120dd8453bb02047022f",
-            "query": this.movieListInput
-          }
-        })
-        .then(data => {
-          this.filmList = data.data.results;
-          console.log(this.filmList);
-          this.movieListInput = "";
-        })
-        .catch(() => console.log("I AM ERRROR"));
+        // axios.get('https://api.themoviedb.org/3/search/tv',{
+        //   params: {
+        //     "api_key": "5417e301ed04120dd8453bb02047022f",
+        //     "query": this.movieListInput
+        //   }
+        // })
+        // .then(data => {
+        //   this.filmList = data.data.results;
+        //   console.log(this.filmList);
+        //   this.movieListInput = "";
+        // })
+        // .catch(() => console.log("I AM ERRROR"));
 
 
 
