3bb74eac7c6351b6059ab9689decf6ddbc9ca010
Danny <danny.brogno@gmail.com>
2021-04-20 13:20:07 +0200

    details added: tv series and pics

--- js/script.js
+++ js/script.js
@@ -15,6 +15,7 @@ function vueInit() {
     data: {
       "movieListInput": "",
       "filmList": [],
+      "series":[],
       flagFilm: {
         "en": "./img/uk.png",
         "it": "./img/ita.png"
@@ -38,18 +39,19 @@ function vueInit() {
         .catch(() => console.log("I AM ERRROR"));
         // ----------------TV--------------------
         // --------------SERIES------------------
-        // axios.get('https://api.themoviedb.org/3/search/tv',{
-        //   params: {
-        //     "api_key": "5417e301ed04120dd8453bb02047022f",
-        //     "query": this.movieListInput
-        //   }
-        // })
-        // .then(data => {
-        //   this.filmList = data.data.results;
-        //   console.log(this.filmList);
-        //   this.movieListInput = "";
-        // })
-        // .catch(() => console.log("I AM ERRROR"));
+        axios.get('https://api.themoviedb.org/3/search/tv',{
+          params: {
+            "api_key": "5417e301ed04120dd8453bb02047022f",
+            "query": this.movieListInput
+          }
+        })
+        .then(data => {
+
+          this.series = data.data.results;
+          console.log(this.series);
+          this.movieListInput = "";
+        })
+        .catch(() => console.log("I AM ERRROR"));
 
 
 
