92b3b8598a1ce76c38ac29787e8726e13e44f3a9
Danny <danny.brogno@gmail.com>
2021-04-20 17:55:34 +0200

    cut text function

--- js/script.js
+++ js/script.js
@@ -55,20 +55,35 @@ function vueInit() {
         })
         .catch(() => console.log("I AM ERRROR"));
 
-      } // END apiFun
+      }, // END apiFun
+
+      // THIS FUNCTION IS TO CU THE H2 TEXT IN EACH BOX WHEN IT'S LONGER THAN (CHECK HTML LINE 41)
+      cutText: function (title, length, suffix) { // I pass the name of this funtion inside the {{ }} in HTML and then I pass it the parameters: title, length (decide how long you want the string to be), and then the suffix "..."
+        if (title.length > length) {
+          return title.substring(0, length) + suffix;
+        } else {
+          return title; // but if the text is no longer than the length I decided then it gives back the length
+        }
+
+      }, // END OF cutText FUNCTION (IN METHODS)-----------------------------
 
     }, // END METHODS
     computed: {
       films: function() {
-        return this.filmList.map((film)=>{
+        return this.filmList.map((film) => {
 
-
-          const movie = {
+          // SPREAD OPERATOR
+          let movie = {
             ...film // this is to NOT edit the original vote, we want to translate the vote from 1/10 to 1/5 not modify the vote_average into the array
           }
 
+          // let tvSeries = {
+          //   ...series
+          // }
+
           movie.srcFlag = this.flagFilm[film.original_language]; // because we have already accessed the "movie" we can add the flag in teh same way
           movie.vote_average = Math.ceil(movie.vote_average / 2 );
+          // tvSeries.vote_average = Math.ceil(tvSeries.vote_average / 2 );
           return movie;
         });
       } // END FILMS FUNCTION
